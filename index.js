3165c0173c9a372f1c8593c0a6f8aaa48885b4e2
ANDRÉ LUIZ CONSTANTINO <andreluis081205@gmail.com>
2023-09-01 13:28:52 -0300

    01/09/ organização

--- index.js
+++ index.js
@@ -46,9 +46,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 app.get('/test', cors(), async (req, res) => {
-    res.json({
-        "teste": "aa"
-    })
+     
 })
 
 app.listen(8080, () => console.log('Servidor rodando na porta 8080'))
